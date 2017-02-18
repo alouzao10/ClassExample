904e19865421f17a4b45751faf9942f49d7d9bf2
Alex Louzao <alexlouzao@desktop-umpqin0.highpoint.edu>
2017-02-18 14:03:16 -0500

    before ch 8

--- QuizApp/ViewController.swift
+++ QuizApp/ViewController.swift
@@ -1,23 +1,23 @@
 //
 //  ViewController.swift
 //  QuizApp
-//
+//  GIT_HUB
 //  Created by Alex Louzao on 1/15/17.
-//  Updated: on 1/23/17 
+//  Updated: on 1/23/17
 //  Copyright © 2017 ALcsc2310. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet var questionLabel: UILabel!
     @IBOutlet var answerLabel: UILabel!
-
+    
     let questions: [String] = ["What is 7 + 7?", "What is the capitol of VT?", "What is cognac made from?"]
     let answers: [String] = ["14", "Montpelier", "Grapes"]
     var currentQuestion: Int = 0
-
+    
     
     @IBAction func showNextQuestion(_ sender: UIButton){
         currentQuestion += 1
@@ -27,16 +27,16 @@ class ViewController: UIViewController {
         let question: String = questions[currentQuestion]
         questionLabel.text = question
         answerLabel.text = "???"
+        
     }
     @IBAction func showAnswer(_ sender: UIButton){
         let answer: String = answers[currentQuestion]
         answerLabel.text = answer
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         questionLabel.text = questions[currentQuestion]
     }
-
+    
 }
-
